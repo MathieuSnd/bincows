fb0f9c1b11f44e7d2e5a2aed44cf9752e1e60093
Mathieu Serandour <mathieu.serandour@telecom-paris.fr>
2021-11-21 14:44:18 +0100

    print stack trace when panicking

--- kernel/debug/panic.c
+++ kernel/debug/panic.c
@@ -5,6 +5,24 @@
 
 int zero = 0;
 
+#define MAX_STACK_TRACE 15
+
+extern uint64_t _rbp(void);
+
+// always inline: make sure 
+static __attribute__((always_inline)) void stack_trace(void) {
+    void** ptr = (void**)_rbp();
+    kputs("backtrace:\n");
+    
+    for(unsigned i = 0; i < MAX_STACK_TRACE; i++) {
+        
+        if(*ptr == 0) // reached the top
+            break;
+        kprintf("\t?? in %llx\n", *(ptr+1));
+
+        ptr = *ptr;
+    }
+}
 
 __attribute__((noreturn)) void panic(const char* panic_string) {
     // checks if video is operationnal
@@ -20,6 +38,10 @@ __attribute__((noreturn)) void panic(const char* panic_string) {
             "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
         
         kputs(panic_string);
+        kputs("\n\n");
+        
+        stack_trace();
+
         kputs(
             "\n\n"
             "you may manually shutdown the machine.\n"
