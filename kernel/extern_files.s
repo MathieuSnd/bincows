98266d8fa174a073bd44f28c54b94304081c33a1
Mathieu Serandour <mathieu.serandour@telecom-paris.fr>
2021-11-21 14:46:07 +0100

    needed stuf for the console

--- kernel/extern_files.s
+++ kernel/extern_files.s
@@ -1,11 +1,21 @@
 [section .rodata]
 [global _binary_charmap_bmp]
 [global _binary_bootmessage_txt]
+[global __image_pix]
 
 _binary_charmap_bmp:
     incbin "../resources/bmp/charmap.bmp"
+_binary_charmap_bmp_end:
 
-    
+
 _binary_bootmessage_txt:
     incbin "../resources/ascii/boot_message.txt"
     db 0
+
+
+[section .bss]
+
+alignb 4
+__image_pix:
+    TIMES 0x2000  resb 0
+    
