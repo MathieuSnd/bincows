ad7ed5171cc760ebd432a2bc0eccb09fc7eb31b5
Mathieu Serandour <mathieu.serandour@telecom-paris.fr>
2021-12-05 11:39:27 +0100

    fixed errors

--- kernel/registers.h
+++ kernel/registers.h
@@ -13,21 +13,16 @@ u16 _gs(void);
 uint64_t get_rflags(void);
 uint64_t get_cr0(void);
 uint64_t _cr2(void);
-uint64_t _cr3(void);
+void     _cr3(uint64_t cr3);
 uint64_t get_cr4(void);
 
 
-u16 set_cs(void);
-u16 set_ds(void);
-u16 set_ss(void);
-u16 set_es(void);
-u16 set_fs(void);
-u16 set_gs(void);
+void set_rflags(uint64_t rf);
+void set_cr0(uint64_t cr0);
+void set_cr4(uint64_t cr4);
 
-void set_rflags(void);
-void set_cr0(void);
-void set_cr4(void);
 
+#define IA32_EFER_MSR 0xC0000080
 
 uint64_t read_msr(uint32_t addr);
 uint64_t write_msr(uint32_t addr, uint64_t value);
