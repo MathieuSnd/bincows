21e6bce738acedca33bf8f53ec71ef62fa8ed5de
Mathieu Serandour <mathieu.serandour@telecom-paris.fr>
2021-12-07 11:51:02 +0100

    fixed error

--- kernel/lib/logging.h
+++ kernel/lib/logging.h
@@ -7,7 +7,7 @@
 
 #define LOG_LEVEL_DEBUG   1
 #define LOG_LEVEL_INFO    2
-#define LOG_LEVEL_WARNING 3
+#define LOG_LEVEL_WARN    3
 
 #define LOG_DEBUG_COLOR   0x909090
 #define LOG_INFO_COLOR    0xc0c0c0
