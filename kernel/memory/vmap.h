8427d36d41e591520ca677f07ec979377b76576a
Mathieu Serandour <mathieu.serandour@telecom-paris.fr>
2021-12-18 19:22:39 +0100

    is_higher_half()

--- kernel/memory/vmap.h
+++ kernel/memory/vmap.h
@@ -38,14 +38,15 @@
  *                     |       ...      |
  *                     | -------------- |
  * 0xffffffff1fffe000  |      HPET      |
+ *                     | -------------- |
  * 0xffffffff1ffff000  |      LAPIC     |
  * 0xffffffff20000000  |----------------|
  * 
  * 
  */
 
-#define HPET_VIRTUAL_ADDRESS 0xffffffff1fffe000llu
-#define APIC_VIRTUAL_ADDRESS 0xffffffff1ffff000llu
+#define HPET_VADDR 0xffffffff1fffe000llu
+#define APIC_VADDR 0xffffffff1ffff000llu
 
 
 
@@ -69,7 +70,7 @@
  * 
  */
 
-
+#define USER_END            0x0000007fffffffff
 #define TRANSLATED_PHYSICAL_MEMORY_BEGIN 0xffff800000000000llu
 #define MMIO_BEGIN          0xffffffff00000000llu
 #define KERNEL_DATA_BEGIN   0xffffffff80000000llu
@@ -77,34 +78,38 @@
 
 // return non 0 value iif the given address
 // resides in kernel memory
-static inline int is_user(uint64_t virtual_address) {
+static inline int is_user(uint64_t vaddr) {
     // user is in the lower half
-    return ! (virtual_address & (1llu << 63));
+    return (vaddr & ~USER_END) == 0;
 }
-static inline int is_allocator_memory(uint64_t virtual_address) {
-    // user is in the lower half
-    return (virtual_address & 0xff80000000000000) == 0xff00000000000000;
-}
-static inline int is_kernel_memory(uint64_t virtual_address) {
+
+static inline int is_kernel_memory(uint64_t vaddr) {
     // kernel is in the 2 higher GB
-    return (virtual_address & KERNEL_DATA_BEGIN) == KERNEL_DATA_BEGIN;
+    return (vaddr & KERNEL_DATA_BEGIN) == KERNEL_DATA_BEGIN;
 }
-static inline int is_mmio(uint64_t virtual_address) {
+
+static inline int is_mmio(uint64_t vaddr) {
     // between -4 GB and -2 GB
-    return (virtual_address & KERNEL_DATA_BEGIN) == MMIO_BEGIN;
+    return (vaddr & KERNEL_DATA_BEGIN) == MMIO_BEGIN;
+}
+
+static inline int is_higher_half(uint64_t vaddr) {
+    // higher half begins at  TRANSLATED_PHYSICAL_MEMORY_BEGIN
+    return (vaddr & TRANSLATED_PHYSICAL_MEMORY_BEGIN) 
+                 == TRANSLATED_PHYSICAL_MEMORY_BEGIN;
 }
 
 // return the physical address of a
 // stivale2 high half pointer
 static inline uint64_t early_virtual_to_physical(
-        const void* virtual_address) {
+        const void* vaddr) {
     
-    if((0xfffff00000000000llu & (uint64_t)virtual_address) ==
+    if((0xfffff00000000000llu & (uint64_t)vaddr) ==
                          TRANSLATED_PHYSICAL_MEMORY_BEGIN)
-        return ~TRANSLATED_PHYSICAL_MEMORY_BEGIN & (uint64_t)virtual_address;
+        return ~TRANSLATED_PHYSICAL_MEMORY_BEGIN & (uint64_t)vaddr;
     
     else
-        return ~KERNEL_DATA_BEGIN & (uint64_t)virtual_address;
+        return ~KERNEL_DATA_BEGIN & (uint64_t)vaddr;
     
 }
 
