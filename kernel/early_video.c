ecca47a96c52ff853940e9a2cb2a9f2c412ef961
Mathieu Serandour <mathieu.serandour@telecom-paris.fr>
2021-12-18 19:18:12 +0100

    video_init()

---
+++ kernel/early_video.c
@@ -0,0 +1,38 @@
+#include <stivale2.h>
+
+#include "early_video.h"
+#include "drivers/terminal/terminal.h"
+#include "memory/vmap.h"
+
+typedef struct framebuffer_dev fd_t;
+
+void video_init(
+        struct stivale2_struct_tag_framebuffer* framebuffer_tag
+) {
+
+// framebuffer virtual device
+    fd_t* fb_dev = 
+                malloc(sizeof(fd_t));
+    *fb_dev = (fd_t){
+        .dev = {
+            .type = DEVICE_ID_FRAMEBUFFER,
+            .name = {"stivale2 framebuffer",0},
+            .driver = NULL
+        },
+        .width  = framebuffer_tag->framebuffer_width,
+        .height = framebuffer_tag->framebuffer_height,
+        .bpp    = framebuffer_tag->framebuffer_bpp,
+        .pitch  = framebuffer_tag->framebuffer_pitch,
+
+        .pix = (void *)MMIO_BEGIN,
+    };
+
+
+    register_dev((struct dev *)fb_dev);
+
+    driver_register_and_install(
+        (int (*)(struct driver*))
+        terminal_install,
+        (struct dev *)fb_dev
+    );
+}
