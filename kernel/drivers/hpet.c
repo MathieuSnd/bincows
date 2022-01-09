4b0c3be971cd7b748db8875cc9e027d9ba3e6f98
Mathieu Serandour <mathieu.serandour@telecom-paris.fr>
2022-01-09 11:45:27 +0100

    HPET MSIs

--- kernel/drivers/hpet.c
+++ kernel/drivers/hpet.c
@@ -4,6 +4,9 @@
 #include "../memory/vmap.h"
 #include "../lib/assert.h"
 #include "../lib/logging.h"
+#include "../lib/panic.h"
+
+
 /**
 000-007h General Capabilities and ID Register Read Only
 008-00Fh Reserved
@@ -29,19 +32,19 @@
  */
 struct HPET_MMIO {
     volatile const uint64_t capabilities;
-    const uint64_t reserved0;
-    volatile uint64_t       general_config;
-    uint64_t reserved1;
-    volatile uint64_t irqstate;
-    uint8_t reserved2[0xf0-0x28];
-    volatile uint64_t main_counter_value;
-    uint64_t reserved3;
-    
-    volatile uint64_t timer0_config; 
-    volatile uint64_t timer0_conparator_value; 
-    volatile uint64_t timer0_FSB_interrupt_route; 
-
-    uint8_t reserved4[0x400 - 0x118];
+             const uint64_t reserved0;
+    volatile       uint64_t general_config;
+                   uint64_t reserved1;
+    volatile       uint64_t irqstate;
+                   uint8_t  reserved2[0xf0-0x28];
+    volatile       uint64_t main_counter_value;
+                   uint64_t reserved3;
+            
+    volatile       uint64_t timer0_config; 
+    volatile       uint64_t timer0_conparator_value; 
+    volatile       uint64_t timer0_FSB_interrupt_route; 
+        
+                   uint8_t  reserved4[0x400 - 0x118];
 };
 
 static_assert(sizeof(struct HPET_MMIO) == 0x400);
@@ -62,11 +65,53 @@ void hpet_prepare_wait_ms(unsigned ms) {
 
 // deactivate hpet timer
     base->general_config &= ~1;
-    base->main_counter_value = - ((uint64_t)ms * 0xe8d4a51000llu / hpet_period);
+    base->main_counter_value = 
+                - ((uint64_t)ms * 0xe8d4a51000llu / hpet_period);
 }
 //t * 1000 / (T * 1e15) * 
 
-void hpet_wait() {
+
+
+void hpet_enable_irq(uint8_t vector) {
+
+    // check that Timer 0 supports FSB interrupts
+    int cap = base->timer0_config & (1 << 15);
+    log_info("CAP: %lx", base->timer0_config);  
+    if(!cap)
+        panic("HPET does not implement FSB interrupts");
+
+
+    int edge_trigger = 0;
+    int deassert     = 0;
+
+
+    uint32_t val  = (vector & 0xFF) 
+          | ((edge_trigger == 1) ? 0 : (1 << 15)) 
+          | ((deassert     == 1) ? 0 : (1 << 14));
+    uint32_t addr = 0xFEE00000;
+
+
+    base->timer0_conparator_value    = 0;
+    base->timer0_FSB_interrupt_route = val | ((uint64_t)addr << 32);
+
+    // enable timer and level triggered FSB interrupts
+    base->timer0_config              = (1 << 14) // Tn_FSB_EN_CNF 
+                                     | (1 << 2)  // Tn_INT_ENB_CNF
+                                     | (1 << 1)  // Tn_INT_TYPE_CNF
+                                     | (base->timer0_config & (
+                                       (1 << 15) // 
+                                     | (1 << 5)  // read only bits
+                                     | (1 << 4)  // 
+                                     ));
+}
+
+
+void hpet_mask_irq(void) {
+    base->timer0_config &= ~(1 << 14); // Tn_FSB_EN_CNF
+}
+
+
+void hpet_wait(void) {
     base->general_config |= 1;
 
     while(base->main_counter_value > 0x1000) {
--- kernel/drivers/hpet.h
+++ kernel/drivers/hpet.h
@@ -1,6 +1,16 @@
 #pragma once
 
+#include <stdint.h>
+
 void hpet_init(void);
 void hpet_prepare_wait_ms(unsigned);
+
+// enables HPET, busy wait until the main counteur
+// reaches 0, and returns without disabling HPET!  
 void hpet_wait(void);
+
+void hpet_enable(void);
 void hpet_disable(void);
+
+void hpet_enable_irq(uint8_t vector);
+void hpet_mask_irq(void);
