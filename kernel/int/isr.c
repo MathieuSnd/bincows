f8c2cb5be07f7833311a0c5c406a18df4ae15182
Mathieu Serandour <mathieu.serandour@telecom-paris.fr>
2022-01-09 13:37:50 +0100

    added some more IRSs

--- kernel/int/isr.c
+++ kernel/int/isr.c
@@ -51,20 +51,21 @@ static void panic_handler_code(
 
 
 #define DECLARE_EXCEPTION_HANDLER(NAME) \
-__attribute__((interrupt))\
+static __attribute__((interrupt))\
 void NAME(struct IFrame* ifr) {\
     panic_handler(#NAME, ifr);\
+    __builtin_unreachable();\
 }
 
 
 #define DECLARE_CODE_EXCEPTION_HANDLER(NAME) \
-__attribute__((interrupt))\
+static __attribute__((interrupt))\
 void NAME(struct IFrame* ifr, uint64_t code) {\
     panic_handler_code(#NAME, ifr, code);\
+    __builtin_unreachable();\
 }
 
 
-DECLARE_EXCEPTION_HANDLER(ISR_general_handler)
 DECLARE_EXCEPTION_HANDLER(ISR_div_by_zero_handler)
 DECLARE_EXCEPTION_HANDLER(ISR_debug_handler)
 DECLARE_EXCEPTION_HANDLER(ISR_NMI_handler)
@@ -76,21 +77,45 @@ DECLARE_EXCEPTION_HANDLER(ISR_device_not_available_handler)
 DECLARE_EXCEPTION_HANDLER(ISR_coproc_segment_overrun_handler)
 DECLARE_EXCEPTION_HANDLER(ISR_LAPIC_spurious)
 
+DECLARE_EXCEPTION_HANDLER(ISR_x87_floating_point_exception_handler)
+DECLARE_EXCEPTION_HANDLER(ISR_machine_check_handler)
+DECLARE_EXCEPTION_HANDLER(ISR_simd_floating_point_exception_handler)
+DECLARE_EXCEPTION_HANDLER(ISR_hypervisor_injection_exception_handler)
+DECLARE_EXCEPTION_HANDLER(ISR_reserved_handler)
+DECLARE_EXCEPTION_HANDLER(ISR_virtualization_exception_handler)
 
-DECLARE_CODE_EXCEPTION_HANDLER(ISR_error_handler)
+
 DECLARE_CODE_EXCEPTION_HANDLER(ISR_double_fault_handler)
 DECLARE_CODE_EXCEPTION_HANDLER(ISR_invalid_TSS_handler)
 DECLARE_CODE_EXCEPTION_HANDLER(ISR_segment_not_present_handler)
 DECLARE_CODE_EXCEPTION_HANDLER(ISR_stack_segment_fault_handler)
 DECLARE_CODE_EXCEPTION_HANDLER(ISR_general_protection_fault_handler)
 
+DECLARE_CODE_EXCEPTION_HANDLER(ISR_alignment_check_handler)
+DECLARE_CODE_EXCEPTION_HANDLER(ISR_control_protection_exception_handler)
+DECLARE_CODE_EXCEPTION_HANDLER(ISR_vmm_communication_exception_handler)
+DECLARE_CODE_EXCEPTION_HANDLER(ISR_security_exception_handler)
 
-__attribute__((interrupt)) void ISR_page_fault_handler(struct IFrame* interrupt_frame, uint64_t error_code) {
+
+
+static __attribute__((interrupt)) 
+void ISR_page_fault_handler(struct IFrame* interrupt_frame, 
+                            uint64_t       error_code) {
     char buff[128];
     //for(;;);
     // the content of cr2 is the illegal address
     sprintf(buff, "PAGE FAULT. illegal address: %16lx, error code %x\n", _cr2(), error_code);
     panic_handler(buff, interrupt_frame);
+    __builtin_unreachable();
+}
+
+
+// not static
+__attribute__((interrupt)) 
+void IRQ_dummy_handler(struct IFrame* interrupt_frame) {
+    (void) interrupt_frame;
+    panic("an unregistered irq number fired\n");
+    __builtin_unreachable();
 }
 
 
@@ -117,13 +142,32 @@ void setup_isrs(void) {
     set_irs_handler(12, ISR_stack_segment_fault_handler);
     set_irs_handler(13, ISR_general_protection_fault_handler);
     set_irs_handler(14, ISR_page_fault_handler);
+    
+    set_irs_handler(15, ISR_reserved_handler);
+    
+    set_irs_handler(16, ISR_x87_floating_point_exception_handler);
+    set_irs_handler(17, ISR_alignment_check_handler);
+    set_irs_handler(18, ISR_machine_check_handler);
+    set_irs_handler(19, ISR_simd_floating_point_exception_handler);
+    set_irs_handler(20, ISR_virtualization_exception_handler);
+    set_irs_handler(21, ISR_control_protection_exception_handler);
+
+    for(unsigned i = 22; i <= 27; i++)
+        set_irs_handler(i, ISR_reserved_handler);
+    
+    set_irs_handler(28, ISR_hypervisor_injection_exception_handler);
+    set_irs_handler(29, ISR_vmm_communication_exception_handler);
+    set_irs_handler(30, ISR_security_exception_handler);
+    
+    set_irs_handler(31, ISR_reserved_handler);
 
 
-    for(int i = 15; i < 256; i++)
-        set_irs_handler(i,ISR_LAPIC_spurious);
+    for(int i = 32; i <= 254; i++)
+        set_irs_handler(i, IRQ_dummy_handler);
 
 
-    set_irs_handler(32, ISR_coproc_segment_overrun_handler);
+    set_irs_handler(255, ISR_LAPIC_spurious);
+
     setup_idt();
     _sti();
 }
