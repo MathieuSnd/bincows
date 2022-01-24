a35a3b825420373457f410940c1fb871cd3f0fa6
Mathieu Serandour <mathieu.serandour@telecom-paris.fr>
2022-01-24 21:53:19 +0100

    vfs_read_file

--- kernel/fs/vfs.c
+++ kernel/fs/vfs.c
@@ -3,39 +3,48 @@
 #include "../lib/string.h"
 #include "../lib/assert.h"
 #include "../lib/logging.h"
+#include "../lib/dump.h"
+#include "../lib/sprintf.h"
+#include "../lib/panic.h"
 
+#include "../memory/heap.h"
 
 #include "fat32/fat32.h"
 
-
 static dirent_t vfs_root;
 
-
-
-static 
-int is_absolute(const char* path) {
+static int is_absolute(const char *path)
+{
     return path[0] == '/';
 }
 
-
-void init_vfs(void) {
-    vfs_root.name[0]    = 0;
-    vfs_root.type       = DT_DIR;
-    vfs_root.children   = NULL;
+void init_vfs(void)
+{
+    vfs_root.name[0] = 0;
+    vfs_root.type = DT_DIR;
+    vfs_root.children = NULL;
     vfs_root.n_children = 0;
-    vfs_root.file_size  = 0;
+    vfs_root.file_size = 0;
 
-// virtual dir
-    vfs_root.fs         = NULL;
+    // virtual dir
+    vfs_root.fs = NULL;
 }
 
+dirent_t *find_child(dirent_t *dir, const char *name)
+{
+    if (dir->children == NULL && dir->fs)
+    {
+        // maybe the children are not
+        // yet cached
+        dir->fs->read_dir(dir->fs, dir);
+    }
 
-dirent_t* find_child(dirent_t* dir, const char*  name) {
-    for(int i = 0; i < dir->n_children; i++) {
+    for (unsigned i = 0; i < dir->n_children; i++)
+    {
 
-        dirent_t* d = &dir->children[i];
+        dirent_t *d = &dir->children[i];
 
-        if(!strcmp(d->name, name))
+        if (!strcmp(d->name, name))
             return d;
     }
 
@@ -43,13 +52,14 @@ dirent_t* find_child(dirent_t* dir, const char*  name) {
     return NULL;
 }
 
-static dirent_t* add_dir_entry(
-        dirent_t* restrict dir, 
-        unsigned char type,
-        const char* name) {
+static dirent_t *add_dir_entry(
+    dirent_t *restrict dir,
+    unsigned char type,
+    const char *name)
+{
 
     unsigned id = dir->n_children;
-    
+
     dir->n_children++;
     dir->children = realloc(dir->children, dir->n_children * sizeof(dirent_t));
 
@@ -65,28 +75,29 @@ static dirent_t* add_dir_entry(
     return &dir->children[id];
 }
 
-
-dirent_t* vfs_open_dir(const char* path, int create) {
+dirent_t *vfs_open_dir(const char *path, int create)
+{
     assert(is_absolute(path));
 
-    char* buf = malloc(strlen(path));
+    char *buf = malloc(strlen(path));
 
     strcpy(buf, path);
 
+    char *sub = strtok(buf, "/");
 
-    char* sub = strtok(buf, "/");
+    dirent_t *dir = &vfs_root;
+    dirent_t *d = dir;
 
-    dirent_t* dir = &vfs_root;
-    dirent_t* d = dir;
-
-
-    while(sub != NULL) {
+    while (sub != NULL)
+    {
         d = find_child(dir, sub);
-        if(!d) {
-            if(create)
+        if (!d)
+        {
+            if (create)
                 d = add_dir_entry(dir, DT_DIR, sub);
-            else {
-            
+            else
+            {
+
                 free(buf);
 
                 return NULL;
@@ -99,80 +110,190 @@ dirent_t* vfs_open_dir(const char* path, int create) {
     }
 
     free(buf);
-    
+
     return d;
 }
 
+void log_tree(dirent_t *dir, int level)
+{
 
-
-void log_tree(dirent_t* dir, int level) {
-    
-    for(int i = 0; i < level+1; i++)
+    for (int i = 0; i < level + 1; i++)
         puts("--");
-    
+
     printf(" %u - %s\n", dir->type, dir->name);
 
-    if(dir->type != DT_DIR)
+    if (dir->type != DT_DIR)
         return;
-    if(!strcmp(dir->name, ".") || !strcmp(dir->name, ".."))
+    if (!strcmp(dir->name, ".") || !strcmp(dir->name, ".."))
         return;
 
-    int n = 0;
-
-    if(dir->type == DT_DIR && !dir->children && dir->fs) {
-        fat32_read_dir(dir->fs, dir, &n);
+    if (dir->type == DT_DIR && !dir->children && dir->fs)
+    {
+        fat32_read_dir(dir->fs, dir);
     }
 
-
-    for(int i = 0; i < dir->n_children; i++)
-        log_tree(&dir->children[i], level+1);
+    for (unsigned i = 0; i < dir->n_children; i++)
+        log_tree(&dir->children[i], level + 1);
 
     // free(children);
 }
 
+int mount(disk_part_t *part, const char *path)
+{
+    dirent_t *dir = vfs_open_dir(path, 1);
 
-int mount(disk_part_t* part, const char* path) {
-    dirent_t* dir = vfs_open_dir(path, 1);
+    fs_t *fs = fat32_detect(part, dir);
 
-    fs_t* fs = fat32_detect(part, dir);
-
-    if(!fs) {
+    if (!fs)
+    {
         log_warn("cannot mount %s, unrecognized format", part->name);
         return 0;
     }
 
-    log_tree(&vfs_root, 0);
-
-    dirent_t* d = vfs_open_file("/a/boot/limine.cfg");
-
-    assert(d);
-
-    vfs_close_file(d);
-
     return 1;
 }
 
+file_handler_t *vfs_open_file(const char *filename)
+{
+    dirent_t *dirent = vfs_open_dir(filename, 0);
 
-void* vfs_open_file(const char* filename) {
-    dirent_t* dir = vfs_open_dir(filename, 0);
-
-    if(!dir || dir->type != DT_REG)
+    // file does not exist or isn't a file
+    if (!dirent || dirent->type != DT_REG)
         return NULL;
 
-    log_warn("dir=%x", dir->name);
+    fs_t *restrict fs = dirent->fs;
+
     // virtual file (doesn't make sens)
-    if(!dir->fs)
-        return NULL; 
-    
-    return dir->fs->open_file(dir);
+    if (!fs)
+        return NULL;
+
+    // big allocation to allocate the
+    // sector buffer too
+    file_handler_t *handler = malloc(
+        sizeof(file_handler_t) 
+        + fs->file_cursor_size
+        // size of one sector
+        + fs->file_access_granularity);
+
+
+    handler->file = dirent;
+    handler->fs = fs;
+    handler->file_offset = 0;
+    // empty buffer
+    handler->sector_offset = fs->file_access_granularity;
+    handler->sector_buff = (void *)handler + 
+                sizeof(file_handler_t) + fs->file_cursor_size;
+
+    fs->open_file(dirent, handler->cursor);
+    return handler;
 }
 
+void vfs_close_file(file_handler_t *handle)
+{
 
-void vfs_close_file(void* handle) {
-    dirent_t** base_handle = handle;
+    dirent_t *file = handle->file;
 
-    fs_t* fs = (**base_handle).fs;
-    
+    fs_t *fs = file->fs;
+
     fs->close_file(handle);
 }
 
+
+dirent_t *read_dir(dirent_t *dir)
+{
+    assert(dir->type == DT_DIR);
+    if (dir->children == NULL && dir->fs)
+        dir->fs->read_dir(dir->fs, dir);
+
+    return dir->children;
+}
+
+
+void free_dir(dirent_t *entries)
+{
+    (void)entries;
+    // nothing
+}
+
+
+size_t vfs_read_file(void *ptr, size_t size, size_t nmemb,
+                     file_handler_t *restrict stream)
+{
+
+    void *buf = stream->sector_buff;
+    fs_t *fs = stream->fs;
+    dirent_t* file = stream->file;
+
+    unsigned granularity = stream->fs->file_access_granularity;
+
+
+    unsigned max_read = file->file_size - stream->file_offset;
+
+    unsigned bsize = size * nmemb;
+
+    // check nmemb bounds
+
+    if(max_read < bsize) {
+        nmemb = max_read / size;
+        bsize = size * nmemb;
+    }
+
+
+    while(bsize) {
+        unsigned remaining = file->file_size - stream->file_offset;
+        
+        unsigned n_ready = granularity - stream->sector_offset;
+
+        if(n_ready > remaining)
+            n_ready = remaining;
+
+
+        if(n_ready >= bsize)
+        {
+            memcpy(
+                ptr,
+                buf + stream->sector_offset,
+                bsize
+            );
+
+            ptr += bsize;
+            stream->sector_offset += bsize;
+            stream->file_offset   += bsize;
+
+            bsize = 0;
+        }
+        else
+        {
+            memcpy(
+                ptr,
+                buf + stream->sector_offset,
+                n_ready
+            );
+
+            bsize -= n_ready;
+
+
+            ptr += n_ready;
+            stream->file_offset += n_ready;
+            stream->sector_offset = 0;
+
+            fs->read_file_sector(
+                stream->fs,
+                stream->cursor,
+                buf);
+        }
+    }
+
+    return nmemb;
+}
+
+
+size_t vfs_write_file(const void *ptr, size_t size, size_t nmemb,
+                      file_handler_t *stream) {
+    (void) ptr;
+    (void) size;
+    (void) nmemb;
+    (void) stream;
+    panic("vfs_write_file: unimplemented");
+    __builtin_unreachable();
+}
--- kernel/fs/vfs.h
+++ kernel/fs/vfs.h
@@ -6,6 +6,39 @@ int mount(disk_part_t* part, const char* path);
 
 void vfs_init(void);
 
+
+typedef struct file_handler {
+    dirent_t* file;
+    fs_t* fs;
+
+    // the filesystems have sector granularity
+    // so we put those fields to buffer the accesses
+    // and have byte granularity
+
+    // byte offset in the current sector
+    // equals fs->file_granularity if the 
+    // buffer is empty
+    unsigned sector_offset;
+
+    // current byte offset in the file
+    uint64_t file_offset;
+
+    // buffer of fs->file_access_granularity
+    // bytes.
+    // keep the current sector buffered
+    void* sector_buff;
+
+
+
+    // this fild's size will depend
+    // be fs->file_cursor_size
+    //
+    // uint64_t: make sure the structure
+    // will be aligned
+    uint64_t cursor[0];
+} file_handler_t;
+
+
 /**
  * @brief open a directory entry
  * 
@@ -20,8 +53,37 @@ void vfs_init(void);
 dirent_t* vfs_open_dir(const char* path, int create);
 
 
-void* vfs_open_file(const char* filename);
-void vfs_close_file(void* handle);
+/**
+ * @brief read a directory
+ * if the dir->children is NULL and dir
+ * doesn't represent a virtual directory,
+ * then this function will find its children
+ * and fill the structure fields.
+ * 
+ * @param dir the directory to read
+ * @return dirent_t* the new dir->children value
+ */
+dirent_t* read_dir(dirent_t* dir);
+
+
+/**
+ * @brief free directory entries returned by
+ * read_dir()
+ * 
+ */
+void free_dir(dirent_t*);
+
+file_handler_t* vfs_open_file(const char* filename);
+void vfs_close_file(file_handler_t* handle);
+
+
+size_t vfs_read_file(void* ptr, size_t size, size_t nmemb, 
+            file_handler_t* stream);
+
+size_t vfs_write_file(const void* ptr, size_t size, size_t nmemb,
+            file_handler_t* stream);
+
+
 
 #define MAX_PATH_LENGTH 1024
 
