5ec94a8eaebb19fca2bd162fa63707a67cc563fe
Mathieu Serandour <mathieu.serandour@telecom-paris.fr>
2022-02-11 13:51:00 +0100

    better assertions

--- kernel/lib/panic.c
+++ kernel/lib/panic.c
@@ -4,7 +4,12 @@
 #include "../drivers/ps2kb.h"
 #include "../acpi/power.h"
 #include "../memory/vmap.h"
+#include "../memory/heap.h"
 #include "stacktrace.h"
+
+
+extern const size_t stack_size;
+extern uint8_t stack_base[];
 
 
 __attribute__((noreturn)) void panic(const char* panic_string) {
@@ -25,7 +30,23 @@ __attribute__((noreturn)) void panic(const char* panic_string) {
     puts(panic_string);
     puts("\n\n");
     
+    
     stacktrace_print();
+
+    printf("heap:  %lx -> %lx (size: %6lx B, %5u KB, %u allocations)\n", 
+            KERNEL_HEAP_BEGIN, 
+            heap_get_brk(), 
+            heap_get_size(),
+            heap_get_size() / 1024,
+            heap_get_n_allocation()
+    );
+    printf("stack: %lx -> %lx (size: %6lx B, %5u KB, %lx B free)\n", 
+            stack_base, 
+            &terminal, 
+            (void*)&terminal - (void*)stack_base, 
+            ((void*)&terminal - (void*)stack_base) / 1024, 
+            (void*)stack_base + stack_size - (void*)&terminal
+    );
 
     puts(
         "\n\n"
