adf55afb6562e8d4649eeee554724d3d44cc058b
Mathieu Serandour <mathieu.serandour@telecom-paris.fr>
2022-03-11 11:13:01 +0100

    sync function

--- kernel/drivers/storage_interface.h
+++ kernel/drivers/storage_interface.h
@@ -18,6 +18,17 @@ struct storage_interface {
                  uint64_t    lba,
                  const void* buf,
                  size_t      count);
+
+    // can have wait timings,
+    // but far less than read
+    // call sync when needing every
+    // operation to terminate
+    void (*async_read)(struct driver *,
+                 uint64_t lba,
+                 void*    buf,
+                 size_t   count);
+
+    void (*sync)(struct driver *);
     
     unsigned capacity;
     
