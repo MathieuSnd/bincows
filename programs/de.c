9b8b790e8ad9b2b4d2b8217ddd22cb68b089e2fb
Mathieu Serandour <mathieu.serandour@telecom-paris.fr>
2022-11-23 12:36:15 +0100

    desktop enviromnment update

--- programs/de.c
+++ programs/de.c
@@ -3,7 +3,42 @@
 #include <assert.h>
 
 
+struct dev_video {
+    unsigned width, height;
+    unsigned pitch;
+    unsigned bpp;
+} info;
+
+
+int fetch_video_info(void) {
+    FILE* f = fopen("/dev/video", "r");
+
+
+    int r = fread(&info, 1, sizeof(info), f);
+
+    if(r != sizeof(info)) {
+        return -1;
+    }
+    
+    // check that the structure size is right
+    fseek(f, 0, SEEK_END);
+
+    if(ftell(f) != sizeof(info))
+        return -1;
+
+    return 0;
+
+}
+
+
 int main(int argc, char** argv) {
+    //int res = fetch_video_info();
+    int res = 0;
+    if(res) {
+        printf("de fatal error: cannot fetch video information");
+    }
+
+
     FILE* file = fopen("/mem/video", "r");
     void* framebuffer;
 
@@ -15,8 +50,8 @@ int main(int argc, char** argv) {
     fclose(file);
 
 
-    while(1) {
-        memset(framebuffer, 0xff, 4 * 1920 * 500);
-    }
+//    while(1) {
+//        //memset(framebuffer, 0xff, info.pitch * (info.height + 1));
+//    }
 
 }
